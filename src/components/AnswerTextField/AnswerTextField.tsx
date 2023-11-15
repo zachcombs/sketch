debf5bfeb26a955e73fc2534cb37e59908211736
Zach Combs <zacharycombstwo@gmail.com>
2023-11-15 01:04:43 -0500

    Made AnswerTextField into a series of textboxes

--- src/components/AnswerTextField/AnswerTextField.tsx
+++ src/components/AnswerTextField/AnswerTextField.tsx
@@ -1,41 +1,86 @@
-import { Box, TextField } from '@mui/material';
+import { Box, TextField, Typography } from '@mui/material';
 import { Dispatch, useEffect, useState } from 'react';
 
-type AnswerTextFieldProps = {
-    correctAnswer: string;
-    setHasCorrectlyAnswered: Dispatch<boolean>;
+const initializeCurrentGuess = (correctAnswer: string) => {
+    const tempArray = Array<string>(correctAnswer.length);
+    [...correctAnswer].forEach((char, index) => {
+        if (char === ' ') {
+            tempArray[index] = ' ';
+        } else if (char === '-') {
+            tempArray[index] = '-';
+        } else {
+            tempArray[index] = '';
+        }
+    });
+
+    return tempArray;
 };
 
 function AnswerTextField({
     correctAnswer,
     setHasCorrectlyAnswered,
-}: AnswerTextFieldProps) {
-    const [currentGuess, setCurrentGuess] = useState('');
+}: {
+    correctAnswer: string;
+    setHasCorrectlyAnswered: Dispatch<boolean>;
+}) {
+    const [currentGuess, setCurrentGuess] = useState(
+        initializeCurrentGuess(correctAnswer)
+    );
 
     useEffect(() => {
-        if (currentGuess === correctAnswer) {
+        if (currentGuess.join('').toLowerCase() === correctAnswer) {
             setHasCorrectlyAnswered(true);
         }
     }, [correctAnswer, currentGuess, setHasCorrectlyAnswered]);
 
-    return (
-        <Box>
-            <TextField
-                onChange={(event) => {
-                    setCurrentGuess(event.target.value);
-                }}
-                sx={{ width: 500 }}
-                variant='standard'
-                inputProps={{
-                    style: {
-                        fontSize: 96,
-                        fontFamily: 'Handwriting_1',
-                        textAlign: 'center',
-                    },
-                }}
-            />
-        </Box>
-    );
+    const answerLetterMap = [...correctAnswer].map((char, index) => {
+        if (char === ' ') {
+            return <Box p={1} sx={{ width: 50, height: 50 }} />;
+        }
+        if (char === '-') {
+            return (
+                <Box p={1} sx={{ width: 50, height: 50 }}>
+                    <Typography
+                        fontFamily={'Roboto_Regular'}
+                        fontSize={48}
+                        textAlign={'center'}
+                        width={50}
+                        height={50}
+                    >
+                        {' '}
+                        -
+                    </Typography>
+                </Box>
+            );
+        }
+
+        return (
+            <Box p={1}>
+                <TextField
+                    key={`answer-textfield-${index}`}
+                    inputProps={{
+                        style: {
+                            fontSize: 48,
+                            fontFamily: 'Roboto_Regular',
+                            textAlign: 'center',
+                            width: 50,
+                            height: 50,
+                        },
+                    }}
+                    onChange={(event) => {
+                        if (event.target.value.length > 1) return;
+                        const items = [...currentGuess];
+                        items[index] = event.target.value.toUpperCase();
+                        setCurrentGuess(items);
+                    }}
+                    value={currentGuess[index]}
+                    {...(index === 0 && { autoFocus: true })}
+                />
+            </Box>
+        );
+    });
+
+    return <Box display='flex'>{answerLetterMap}</Box>;
 }
 
 export default AnswerTextField;
